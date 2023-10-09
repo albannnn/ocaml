d8d2f75013ad931cba590c5b9cb4088fc2ee62c1
albannnn <80924616+albannnn@users.noreply.github.com>
2023-10-09 14:39:03 +0200

    Add files via upload

---
+++ TP3.ml
@@ -0,0 +1,56 @@
+(* Exercice 1 *)
+type coloration = 
+|Cyan 
+|Magenta 
+|Jaune
+|Melange of coloration * coloration ;; 
+
+let rouge = Melange(Magenta, Jaune) ;;
+let orange = Melange(rouge, Jaune) ;;
+
+(* Exercice 2 *)
+type 'a liste = 
+  |Nil 
+  |TeteEtQueue of 'a * 'a liste
+;;
+let liste1 = TeteEtQueue(1, TeteEtQueue(2, TeteEtQueue(3, Nil))) ;; 
+let cons tete queue = 
+  TeteEtQueue (tete, queue) ;;
+
+let liste2 =  cons(0, liste1) ;; 
+
+let tete liste = 
+  match liste with
+  |Nil -> failwith "une liste vide n'a pas de tete" ;
+  |TeteEtQueue(tete, queue) -> tete ;;
+
+  let queue liste = 
+    match liste with
+    |Nil -> failwith "une liste vide n'a pas de queue" ;
+    |TeteEtQueue(tete, queue) -> queue ;;
+  
+let rec taille liste = 
+  match liste with
+  |Nil -> 0 ;
+  |TeteEtQueue(tete, queue) -> 1 + taille(queue) ;; 
+
+let rec appartient liste elt = 
+  match liste with 
+  |Nil -> false;
+  |TeteEtQueue(tete, _) when tete = elt -> true ;
+  |TeteEtQueue(_, queue) -> appartient queue elt ;;
+
+let rec supprime liste elt =
+  match liste with
+  |Nil -> Nil ;
+  |TeteEtQueue(tete, queue) -> 
+    if tete = elt then 
+      supprime queue elt 
+    else
+      TeteEtQueue(tete, supprime(queue)(elt)) ;;
+
+let rec element_no liste no =
+  match liste with
+  |Nil -> failwith "es gibt ein problem " ;
+  
+
